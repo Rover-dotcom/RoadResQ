c9262982701115bfdf7461c9edfdf711666a6d1e
Rover-dotcom <hamza1124@yahoo.com>
2026-04-23 15:02:58 +0300

    Integrate admin routes into the server
    Added admin routes to the server.

--- RoadResQ/backend/server.js
+++ RoadResQ/backend/server.js
@@ -4,6 +4,8 @@ const connectDB = require("./config/db");
 
 connectDB();
 
+app.use("/api/admin", require("./routes/adminRoutes"));
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
