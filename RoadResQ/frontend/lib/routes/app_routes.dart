76d4ddef1f47362ed69a17a3b2ba2cf09b25fa64
Rover-dotcom <hamza1124@yahoo.com>
2026-04-23 16:35:58 +0300

    Add app_routes.dart for application routing

---
+++ RoadResQ/frontend/lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../screens/home/home_screen.dart';
+
+class AppRoutes {
+  static Map<String, WidgetBuilder> routes = {
+    '/': (context) => const HomeScreen(),
+  };
+}
